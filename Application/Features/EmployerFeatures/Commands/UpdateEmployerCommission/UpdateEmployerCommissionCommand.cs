d379a4194853423f630c8aa362e9ba0a6ac01330
Amirsharifi0 <fukinfukdup@gmail.com>
2022-08-31 14:52:29 +0430

    Implement Update And Delete Operation Handlers For EmployerCommission And Implement In Controller

---
+++ Application/Features/EmployerFeatures/Commands/UpdateEmployerCommission/UpdateEmployerCommissionCommand.cs
@@ -0,0 +1,8 @@
+﻿using Application.Models.Common;
+using Domain.WriteModel;
+using MediatR;
+
+namespace Application.Features.EmployerFeatures.Commands
+{
+    public record UpdateEmployerCommissionCommand(long employerCommissionId, bool isFixed,int value) : IRequest<OperationResult<EmployerCommission>>;
+}
---
+++ Application/Features/EmployerFeatures/Commands/UpdateEmployerCommission/UpdateEmployerCommissionCommandHandler.cs
@@ -0,0 +1,45 @@
+﻿using Application.BackgroundWorker.Common.Events;
+using Application.Common.BaseChannel;
+using Application.Contracts.Persistence;
+using Application.Models.Common;
+using Domain.WriteModel;
+using MediatR;
+
+namespace Application.Features.EmployerFeatures.Commands
+{
+    internal class UpdateEmployerCommissionCommandHandler : IRequestHandler<UpdateEmployerCommissionCommand, OperationResult<EmployerCommission>>
+    {
+        readonly IUnitOfWork _unitOfWork;
+        private readonly ChannelQueue<EmployerCommisionUpdated> _channel;
+        public UpdateEmployerCommissionCommandHandler(IUnitOfWork unitOfWork, ChannelQueue<EmployerCommisionUpdated> channel)
+        {
+            _unitOfWork = unitOfWork;
+            _channel = channel;
+        }
+        public async Task<OperationResult<EmployerCommission>> Handle(UpdateEmployerCommissionCommand request, CancellationToken cancellationToken)
+        {
+
+            var fetchedEmployerCommission = await _unitOfWork.EmployerCommissionRepository.GetEmployerCommissionByIdAsync(request.employerCommissionId);
+
+            if (fetchedEmployerCommission is null)
+                return OperationResult<EmployerCommission>.FailureResult("The Employer Commission Is Not Exist");
+
+            fetchedEmployerCommission.IsFixed = request.isFixed;
+            fetchedEmployerCommission.Value = request.value;
+
+            var result = await _unitOfWork.EmployerCommissionRepository.UpdateEmployerCommissionAsync(fetchedEmployerCommission);
+            try
+            {
+                await _unitOfWork.CommitAsync();
+
+                await _channel.AddToChannelAsync(new EmployerCommisionUpdated { EmployerCommisionId = fetchedEmployerCommission.Id }, cancellationToken);
+            }
+            catch (Exception ex)
+            {
+                var exception = ex.Message;
+            }
+
+            return OperationResult<EmployerCommission>.SuccessResult(fetchedEmployerCommission);
+        }
+    }
+}
